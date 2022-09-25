c5e722c227dfdb77e37117202d4d3fb16ce82909
ukioxz <ikatember@gmail.com>
2022-09-25 20:08:39 +0300

    Add all icon of characters

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-function showDiv() {
+function showDivArataki() {
    document.getElementById('img-Arataki').style.display = "block";
+   document.getElementById('img-Diluc').style.display = "none";
+   document.getElementById('img-Keya').style.display = "none";
+   document.getElementById('img-Ayato').style.display = "none";
+   document.getElementById('img-Zhongli').style.display = "none";
+}
+function showDivDiluc() {
+   document.getElementById('img-Arataki').style.display = "none";
+   document.getElementById('img-Diluc').style.display = "block";
+   document.getElementById('img-Keya').style.display = "none";
+   document.getElementById('img-Ayato').style.display = "none";
+   document.getElementById('img-Zhongli').style.display = "none";
+}
+function showDivKeya() {
+   document.getElementById('img-Arataki').style.display = "none";
+   document.getElementById('img-Diluc').style.display = "none";
+   document.getElementById('img-Keya').style.display = "block";
+   document.getElementById('img-Ayato').style.display = "none";
+   document.getElementById('img-Zhongli').style.display = "none";
+}
+function showDivAyato() {
+   document.getElementById('img-Arataki').style.display = "none";
+   document.getElementById('img-Diluc').style.display = "none";
+   document.getElementById('img-Keya').style.display = "none";
+   document.getElementById('img-Ayato').style.display = "block";
+   document.getElementById('img-Zhongli').style.display = "none";
+}
+function showDivZhongli() {
+   document.getElementById('img-Arataki').style.display = "none";
+   document.getElementById('img-Diluc').style.display = "none";
+   document.getElementById('img-Keya').style.display = "none";
+   document.getElementById('img-Ayato').style.display = "none";
+   document.getElementById('img-Zhongli').style.display = "block";
 }
